3253048fbf7d21896798bfe39e26d10091e32936
Anca <anca.m.gozman@gmail.com>
2024-04-20 15:02:20 +0200

    minor adjustments

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-myFlix-'`, () => {
+  it(`should have as title 'Angular-myFlix'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-myFlix-');
+    expect(app.title).toEqual('Angular-myFlix');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-myFlix-');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-myFlix');
   });
 });
